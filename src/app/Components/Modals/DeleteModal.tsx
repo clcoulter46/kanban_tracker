a2080494e0b32dcb9798c411a2492451200019aa
clcoulter46 <clcoulter46@gmail.com>
2026-01-19 15:43:29 -0500

    fixed DeleteModal typing errors

--- src/app/Components/Modals/DeleteModal.tsx
+++ src/app/Components/Modals/DeleteModal.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 
-export default function DeleteModal({
-    id,
-    onConfirmDelete,
-    onBackClick
-}) {
+export interface Props {
+    id: string,
+    onConfirmDelete: (event: React.MouseEvent<HTMLElement>) => void,
+    onBackClick: (event: React.MouseEvent<HTMLElement>, id: number) => void,
+}
 
+const DeleteModal: React.FC<Props> = props => {
     return (
         <div className="delete-modal" id="delete-modal">
             <div>Are you sure you want to delete this task?</div>
             <div className="evenly-spaced-buttons">
-                <button onClick={onBackClick} className="button">Back</button>
-                <button onClick={() => onConfirmDelete(id)} style={{color: "red"}} className="button">Confirm</button>
+                {/* @ts-expect-error event handler error */}
+                <button onClick={props.onBackClick} className="button">Back</button>
+                {/* @ts-expect-error event handler error */}
+                <button onClick={() => props.onConfirmDelete(props.id)} style={{color: "red"}} className="button">Confirm</button>
             </div>
         </div>
     )
 }
+
+export default DeleteModal
